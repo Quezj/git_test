f99639023abd4c7778113d51c8e4c269e97ea65f
Quezj <88068986+Quezj@users.noreply.github.com>
2021-10-18 17:55:38 +0800

    modify error commit

--- src/main/java/com/hnucm/git/GitTest.java
+++ src/main/java/com/hnucm/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("master change");
         System.out.println("hello hot-fix");
         System.out.println("push test");
-        System.out.prinltn("pull test");
+        System.out.println("pull test");
     }
 }
