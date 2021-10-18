1460c9cafe3653f7a74f694a40408f192fea6d27
admin <2702780756@qq.com>
2021-10-18 17:22:46 +0800

    hot-fix commit 1

--- src/main/java/com/hnucm/git/GitTest.java
+++ src/main/java/com/hnucm/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hell0 git3");
+        System.out.println("hello hot-fix");
     }
 }
